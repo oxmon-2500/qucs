e31093cde08317987b6918c9640838c4cd656833
Felix Salfelder <felix@salfelder.org>
2018-08-01 09:39:08 +0100

    add items to scene.
    this is preliminary, and does not work

--- qucs/qucs/schematic_file.cpp
+++ qucs/qucs/schematic_file.cpp
@@ -654,6 +654,9 @@ void Schematic::simpleInsertComponent(Component *c)
   }
 
   DocComps.append(c);
+
+  // add Component to scene
+  scene()->addItem(new ElementGraphics(c));
 }
 
 // -------------------------------------------------------------
