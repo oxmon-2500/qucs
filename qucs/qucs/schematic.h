917b62a4bd0bf32f4c8ed784755c6235687cf5ee
Felix Salfelder <felix@salfelder.org>
2018-08-23 17:35:48 +0100

    compile fix

--- qucs/qucs/schematic.h
+++ qucs/qucs/schematic.h
@@ -198,6 +198,8 @@ public: // model
 	  assert(Components);
 	  return *Components;
   }
+  Component* find_component(QString const&);
+
   NodeList& nodes() const{
 	  assert(Nodes);
 	  return *Nodes;
