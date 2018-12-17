bacb4ce1e967589e6092aca5af2d8702fbee4f67
Felix Salfelder <felix@salfelder.org>
2018-12-17 22:58:08 +0000

    simpleInsert fix

--- qucs/qucs/schematic_lang.cpp
+++ qucs/qucs/schematic_lang.cpp
@@ -13,48 +13,49 @@ public:
 	}
 private:
 	void parse(DocumentStream& stream, SchematicModel* s) const {
-
 		QString Line;
 		char mode='\0';
 		while(!stream.atEnd()) {
 			Line = stream.readLine();
+			Line = Line.trimmed();
+			qDebug() << "LSL::parse" <<  Line;
 			if(Line.at(0) == '<'
 			  && Line.at(1) == '/'){
 				incomplete();
-				continue;
-			}
-			Line = Line.trimmed();
-			if(Line.isEmpty()){
-				continue;
+			}else if(Line.isEmpty()){
 			}else if(Line == "<Components>") {
 				mode='C';
 			}else if(Line == "<Wires>") {
 				mode='W';
 			}else if(Line == "<Diagrams>") { untested();
+				mode='D';
 			}else if(Line == "<Paintings>") { untested();
-			}
+				mode='P';
+			}else{
 
-			/// \todo enable user to load partial schematic, skip unknown components
-			Element*c=NULL;
-			if(mode=='C'){
-				c = getComponentFromName(Line, NULL /*???*/);
-			}else if(mode=='W'){
-				// (Node*)4 =  move all ports (later on)
-				Wire* w = new Wire(0,0,0,0, (Node*)4,(Node*)4);
-				c = w->obsolete_load(Line);
-				if(!c){
-					delete(w);
+				/// \todo enable user to load partial schematic, skip unknown components
+				Element*c=NULL;
+				if(mode=='C'){
+					c = getComponentFromName(Line, NULL /*???*/);
+				}else if(mode=='W'){
+					// (Node*)4 =  move all ports (later on)
+					Wire* w = new Wire(0,0,0,0, (Node*)4,(Node*)4);
+					c = w->obsolete_load(Line);
+					if(!c){
+						qDebug() << "ERROR" << Line;
+						delete(w);
+					}else{
+					}
+				}else{
+					incomplete();
+				}
+
+				if(c){
+					s->pushBack(c);
 				}else{
 				}
-			}else{
-				incomplete();
-			}
 
-			if(c){
-				s->pushBack(c);
-			}else{
 			}
-
 		}
 	}
 }defaultSchematicLanguage_;
